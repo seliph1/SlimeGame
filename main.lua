da60be37a17c5cb7834f01fbee5f7b4118981f1a
Mozilla <gabriel_costa_mendes@hotmail.com>
2026-02-12 17:12:08 -0300

    Added UI system (loveframes)

--- main.lua
+++ main.lua
@@ -1,8 +1,44 @@
+local loveframes = require "lib.loveframes"
+
 function love.load()
 end
 
-function love.update(dt)
+function love.update( dt )
+	loveframes.update(dt)
 end
 
 function love.draw()
+	loveframes.draw()
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+	loveframes.mousepressed(x, y, button, istouch, presses)
+	if loveframes.GetInputObject() == false and loveframes.GetCollisionCount() < 1 then
+	end
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+	loveframes.mousereleased(x, y, button, istouch, presses)
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+	loveframes.mousemoved(x, y, dx, dy, istouch)
+end
+
+function love.wheelmoved(x, y)
+	loveframes.wheelmoved(x, y)
+end
+
+function love.keypressed(key, unicode)
+	loveframes.keypressed(key, unicode)
+	if not loveframes.GetInputObject() then
+	end
+end
+
+function love.keyreleased(key, unicode)
+	loveframes.keyreleased(key)
+end
+
+function love.textinput(text)
+	loveframes.textinput(text)
 end
